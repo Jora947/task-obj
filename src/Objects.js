65cd2b9071a12c08ed7e899efcfc5cee03519c83
BuildTools <unconfigured@null.spigotmc.org>
2022-03-30 15:20:08 +0500

    Задание по объектам

--- src/Objects.js
+++ src/Objects.js
@@ -8,19 +8,42 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    if (data.gender === 'female') {
+        delete data.age;
+    }
+    if (data.gender === 'male') {
+        if (data.hasOwnProperty('income') === false) {
+            data.income = 100000;
+        }
+    }
+    return data;
 }
 
 /*
-  В функцию objectFieldsList приходят три объекта с различными полями, список которых заранее неизвестен.
-  Верните список названий этих полей в алфавитном порядке в виде массива строк.
+В функцию objectFieldsList приходят три объекта с различными полями, список которых заранее неизвестен.
+Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    let total_obj = Object.assign({}, obj1, obj2, obj3);
+    let array = [];
+    for (const i in total_obj) {
+        array.push(i);
+    }
+    return array.sort();
 }
 
 /*
-  Верните в результате работы функции массив с клонами объекта obj.
-  При этом каждый клон должен дополнительно содержать поле id со своим порядковым номером в массиве.
-  Количество клонов - count.
+Верните в результате работы функции массив с клонами объекта obj.
+При этом каждый клон должен дополнительно содержать поле id со своим порядковым номером в массиве.
+Количество клонов - count.
 */
 export function objectClone(obj, count) {
+    let arr = [];
+    for (var i = 0; i < count; i++) {
+        let obj1 = obj.constructor();
+        Object.assign(obj1, obj);
+        obj1.id = i;
+        arr.push(obj1);
+    }
+    return arr;
 }
